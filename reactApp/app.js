fe77348a2de6b0eac9e203daa97f36d233eba52e
arzumm <am2692@cornell.edu>
2019-07-18 17:15:25 -0400

    react components

--- reactApp/app.js
+++ reactApp/app.js
@@ -1,7 +1,37 @@
 import React from 'react'; 
 import ReactDOM from 'react-dom'; 
 
+let dummyData = ['do the dishes', 'study for GRE', 'work on React app']; 
+
+class TodoList extends React.Component {
+	constructor(props) {
+		super(props); 
+	}
+
+	render() {
+		return (
+			<ul> 
+			{dummyData.map((item) => <Todo task = {item}/>)} 
+			</ul>  ); 
+	}
+}
+
+class Todo extends React.Component {
+	constructor(props) {
+		super(props); 
+	}
+
+	render() {
+		return (
+		<li> 
+		<input type = 'submit' value = 'X'></input> {this.props.task}  
+		</li>
+		); 
+
+	}
+}
 
 
-ReactDOM.render(<p>React lives!</p>,
-   document.getElementById('root'));
+
+ReactDOM.render(<TodoList />, document.getElementById('root'));
+
