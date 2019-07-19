588b259bdb43ab979757e8dc3815c28503eca152
arzumm <am2692@cornell.edu>
2019-07-19 14:47:24 -0400

    inputting todos

--- reactApp/components/InputLine.js
+++ reactApp/components/InputLine.js
@@ -4,14 +4,26 @@ import ReactDOM from 'react-dom';
 
 class InputLine extends React.Component {
 	constructor(props) {
-		super(props); 
-	}
+        super(props); 
+        this.state = {
+            typedText :''
+        }
+    }
+    
+    handleTyping(event) {
+        this.setState({typedText: event.target.value}); 
+    }
+
+    handleSubmit() {
+        this.props.submit(this.state.typedText); 
+        this.setState({typedText: ''}); 
+    }
 
 	render()  {
 		return (
 		<div> 
-			<input type = "text" name = "todoItem" placeholder = "Enter a todo item" value = ""/> 
-			<input type = 'submit' value = 'Submit' onClick = {() => this.props.submit('sleep')}/> 
+			<input type = "text" name = "todoItem" placeholder = "Enter a todo item" onChange = {(event) => this.handleTyping(event)}/> 
+			<input type = 'submit' value = 'Submit' onClick = {() => this.handleSubmit()}/> 
 		</div> 
 		); 
 	}
